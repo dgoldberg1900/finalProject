c23eae1ba4ee87cb2260ce1a125dc163a6078bb7
Daniel Goldberg <dgoldbe2@stevens.edu>
2024-12-04 16:09:35 -0500

    Added code for kalmanfilter.py and sample code for simulation_runner.py

--- finalProject/src/kalman_filter.py
+++ finalProject/src/kalman_filter.py
@@ -1,1 +1,40 @@
+import numpy as np
+
+# Define the Kalman Filter class
+class KalmanFilter:
+    def __init__(self, A, B, H, Q, R, x0, P0):
+        self.A = A  # State transition matrix
+        self.B = B  # Control input matrix
+        self.H = H  # Observation matrix
+        self.Q = Q  # Process noise covariance
+        self.R = R  # Measurement noise covariance
+        self.x = x0  # Initial state estimate
+        self.P = P0  # Initial covariance estimate
+
+    def predict(self, u):
+        # Predict the next state
+        self.x = np.dot(self.A, self.x) + np.dot(self.B, u)
+        self.P = np.dot(np.dot(self.A, self.P), self.A.T) + self.Q
+
+    def update(self, z):
+        # Update the estimate via measurement
+        y = z - np.dot(self.H, self.x)
+        S = np.dot(self.H, np.dot(self.P, self.H.T)) + self.R
+        K = np.dot(np.dot(self.P, self.H.T), np.linalg.inv(S))
+        self.x = self.x + np.dot(K, y)
+        I = np.eye(self.A.shape[0])
+        self.P = np.dot((I - np.dot(K, self.H)), self.P)
+
+    def get_state(self):
+        return self.x
+
+# Function to run the Kalman Filter (to be called by simulation_runner.py)
+def run_kalman_filter(A, B, H, Q, R, x0, P0, measurements, control_input):
+    kf = KalmanFilter(A, B, H, Q, R, x0, P0)
+    estimates = []
+    for z in measurements:
+        kf.predict(control_input)
+        kf.update(z)
+        estimates.append(kf.get_state()[0])
+    return estimates
  
--- finalProject/src/simulation_runner.py
+++ finalProject/src/simulation_runner.py
@@ -1,1 +1,29 @@
+from kalman_filter import run_kalman_filter
+import numpy as np
+
+# WIP, only calls the Kalman Filter code as of right now, edit to also call the particle filter code and run the two simulations
+# Example parameters for Kalman Filter
+A = np.array([[1]])  # State transition (position update)
+B = np.array([[1]])  # Control input (velocity)
+H = np.array([[1]])  # Observation matrix
+Q = np.array([[0.1]])  # Process noise covariance
+R = np.array([[1]])  # Measurement noise covariance
+x0 = np.array([0])  # Initial state estimate
+P0 = np.array([[1]])  # Initial covariance estimate
+control_input = np.array([1])
+
+# Generate ground truth and measurements
+n_timesteps = 50
+true_position = [0]
+measurements = []
+np.random.seed(42)
+
+for t in range(1, n_timesteps):
+    true_position.append(true_position[-1] + 1 + np.random.normal(0, 0.5))  # Velocity is 1
+    measurements.append(true_position[-1] + np.random.normal(0, 1))
+
+# Run the Kalman Filter using the imported function
+estimates = run_kalman_filter(A, B, H, Q, R, x0, P0, measurements, control_input)
+
+# Further analysis or visualization (e.g., comparing true positions, measurements, estimates)
  
